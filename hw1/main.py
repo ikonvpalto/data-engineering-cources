54a7c428d52bed5dd675213d378aa1b595f9bfad
Valeriy Zakharenko <konvpalto87@gmail.com>
2021-08-08 18:23:44 +0300

    Apply remarks for first homework

--- hw1/main.py
+++ hw1/main.py
@@ -1,14 +1,18 @@
 from stock_api import authorize, get_out_of_stock_info
-from date_utils import get_date_range
+from date_utils import get_date_range, DATE_FORMAT
 from config import get_config
 from store import store_out_of_stock_info
+import click
 
 
-def main():
+@click.command()
+@click.argument("start_date", type=click.DateTime())
+@click.argument("end_date", type=click.DateTime())
+def main(start_date, end_date):
     config = get_config()
     authorize_data = authorize(config)
 
-    for date in get_date_range(config["dates_of_interest"]["start"], config["dates_of_interest"]["end"]):
+    for date in get_date_range(start_date, end_date):
         out_of_stock_info = get_out_of_stock_info(config, authorize_data, date)
         store_out_of_stock_info(config, out_of_stock_info, date)
 
--- hw1/stock_api.py
+++ hw1/stock_api.py
@@ -4,29 +4,33 @@ from date_utils import dump_date
 
 
 def authorize(config):
-    url = prepare_url(config, "auth")
+    endpoint = "auth"
+
+    url = prepare_url(config, endpoint)
     headers = prepare_headers()
     data = json.dumps(config["auth_data"])
 
-    response = requests.post(url, headers=headers, data=data)
+    response = requests.post(url, headers=headers, data=data, timeout=config["endpoints"][endpoint]["timeout"])
     response.raise_for_status()
 
     return "JWT " + response.json()["access_token"]
 
 
 def get_out_of_stock_info(config, authorize_data, date):
-    url = prepare_url(config, "out_of_stock")
+    endpoint = "out_of_stock"
+
+    url = prepare_url(config, endpoint)
     headers = prepare_headers(authorize_data)
     data = json.dumps({"date": dump_date(date)})
 
-    response = requests.get(url, headers=headers, data=data)
+    response = requests.get(url, headers=headers, data=data, timeout=config["endpoints"][endpoint]["timeout"])
     response.raise_for_status()
 
     return response.json()
 
 
 def prepare_url(config, endpoint):
-    return config["url"] + config["endpoints"][endpoint]
+    return config["url"] + config["endpoints"][endpoint]["address"]
 
 
 def prepare_headers(authorization_data=None):
